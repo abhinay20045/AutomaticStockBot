60a89fd50c0dc1a69ed085940782c943bdf75ecc
abhinayprabhuraj <abhinayprabhuraj@gmail.com>
2024-12-18 12:02:46 -0500

    inital commit

--- wb_scraper.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service 
-website = ""
-path = "/Users/abhinayprabhuraj/Downloads/chrome-mac-arm64"
-service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service)
-driver.get(website)
--- ws.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-df = pd.read_csv('https://drive.google.com/uc?id=1zO8ekHWx9U7mrbx_0Hoxxu6od7uxJqWw&export=download')
-df.rename(columns={'First Name':'Fname'})
-print(df)
